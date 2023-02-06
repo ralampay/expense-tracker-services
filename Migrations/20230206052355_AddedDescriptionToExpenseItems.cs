0482946e2c9b662040e5583562fb7870130492ea
Raphael Alampay <raphael.alampay@gmail.com>
2023-02-06 13:29:42 +0800

    Added new migrations

---
+++ Migrations/20230206052355_AddedDescriptionToExpenseItems.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace expensetrackerservices.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDescriptionToExpenseItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "ExpenseItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "ExpenseItems");
+        }
+    }
+}
